c497b83060be822933b8247a7b1f03f6e2071673
John McCabe-Dansted <gmatht@gmail.com>
2026-07-28 05:56:11 +0000

    Bless commit hash to c887501 (21 tests upgraded)

--- ensure_examples_snapshot.pl
+++ ensure_examples_snapshot.pl
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use FindBin;
 
-my $blessed_commit = '4a40a86ecedfab2f15b8db32ea0b07c0a852bd57';
+my $blessed_commit = 'c8875018939152032bad249bc99c37821b8531b3';
 my $cmd = shift @ARGV // '';
 
 if ($cmd eq 'capture') {
