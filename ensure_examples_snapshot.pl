a45302da974dd775e0215d519fea154849a525b4
John McCabe-Dansted <gmatht@gmail.com>
2026-07-28 06:33:36 +0000

    Bless commit hash (3 examples now display variables)

--- ensure_examples_snapshot.pl
+++ ensure_examples_snapshot.pl
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use FindBin;
 
-my $blessed_commit = '576f38cde71a5640ca16c58157260cc06940cd91';
+my $blessed_commit = 'e6aacc9a0a06db7eaf0d16e4f60cc2940b5859e5';
 my $cmd = shift @ARGV // '';
 
 if ($cmd eq 'capture') {
