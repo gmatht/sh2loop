584e75497c44592c1b482ce16a909c386e0f5583
John McCabe-Dansted <gmatht@gmail.com>
2026-07-28 08:45:29 +0000

    Bless commit hash (ideom/ workflow added)

--- ensure_examples_snapshot.pl
+++ ensure_examples_snapshot.pl
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use FindBin;
 
-my $blessed_commit = '16e0436c536a7c9b48f273eac7228267d1c9890b';
+my $blessed_commit = '479f7f89c16edee515d83e9759a93c6621908700';
 my $cmd = shift @ARGV // '';
 
 if ($cmd eq 'capture') {
