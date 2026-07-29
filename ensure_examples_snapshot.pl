ea76083a90c30def8fcf1153b2587df8946c4348
John McCabe-Dansted <gmatht@gmail.com>
2026-07-29 06:16:19 +0000

    Bless examples at e428b13 (readlink -f example, zstd/gunzip examples)

--- ensure_examples_snapshot.pl
+++ ensure_examples_snapshot.pl
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use FindBin;
 
-my $blessed_commit = 'b492eb026b979d78cf4e728053e0765cfc601c59';
+my $blessed_commit = 'e428b139a3140575cc59792a7e1d87138452e7af';
 my $cmd = shift @ARGV // '';
 
 if ($cmd eq 'capture') {
