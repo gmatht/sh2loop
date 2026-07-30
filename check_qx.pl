d67212c6a7a47ba7f9b222528154d410c6dd4f30
John McCabe-Dansted <gmatht@gmail.com>
2026-07-30 02:47:15 +0000

    check_qx: add dd to builtins; update csf failure count to 1

--- check_qx.pl
+++ check_qx.pl
@@ -23,7 +23,7 @@ my @builtins = qw(
     command env
     basename dirname expr hostname id
     readlink realpath uname whoami tty stat
-    gunzip zstd execvp
+    gunzip zstd execvp dd
 );
 
 # Paths relative to this script's location (project root).
