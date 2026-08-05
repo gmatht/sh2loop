5c22702e4244774906ff406895a4b326be844768
John McCabe-Dansted <gmatht@gmail.com>
2026-08-05 10:18:08 +0000

    frontend py-sh-go: gate green

--- harness/.estree-run-nSBo37/prog.mjs
+++
@@ -1,6 +0,0 @@
-import { sh2 } from "/nvme/ai/sh2loop/harness/sh2-namespace.mjs";
-sh2._init("/nvme/ai/sh2loop/sh2perl/examples/arithmetic-var-ref.sh", []);
-sh2._setAllowlist(["/bin/bash","Minimal","reproduction","of","arithmetic","expression","with","variable","references","Similar","to","git-submodule","/","growpart","failures","var","+","1"]);
-process.stdout.write([String((Number(sh2.getVar("var")) || 0) + 1)].join(" ") + "\n");
-
-await sh2._finish();
