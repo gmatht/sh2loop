29c7c1cbc7769bd5326a1d44f4220f3aba37c962
John C. McCabe-Dansted <mccabedj@proton.me>
2026-08-13 15:11:10 +0800

    frontend zig-sh-go: gate green

--- harness/estree_gate.pl
+++ harness/estree_gate.pl
@@ -21,6 +21,7 @@ my %whitelist = map { $_ => 1 } qw(
     define subshell background block whileLoop whileLoopSync whileLoopBatch cstyleFor cstyleForSync forLoop forLoopSync forLoopBatch listVar and or
     subshellSync blockSync captureSync captureWordsSync pipelineSync redirectSync
     shopt return break continue unsupported
+    pipelineInputLines
     trimCapture dirname basename uname date readlink hostname whoami mktempValue split readFile writeFile appendFile lstat access unlink rm mkdir mkdtemp addrOf memLoad memStore
     memAlloc memFree ternary arrayStore memAdvance memTest line
     assocSet assocGet assocNames assocValues
